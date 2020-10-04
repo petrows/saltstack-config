23dc1e411aa50299d1f896cf106f2923cee499eb
Peter Golovachev <petro@petro.ws>
2020-10-04 18:49:37 +0200

    Set powerline and fzf init only for interactive session

--- saltstack/salt/files/linux-config/home/.config/fish/config.fish
+++ saltstack/salt/files/linux-config/home/.config/fish/config.fish
@@ -1,18 +1,20 @@
-# Include powerline (if exists)
-if test -d /usr/share/powerline
-    set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
-    source /usr/share/powerline/bindings/fish/powerline-setup.fish
-    powerline-setup
-end
+if status --is-interactive
+    # Include powerline (if exists)
+    if test -d /usr/share/powerline
+        set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
+        source /usr/share/powerline/bindings/fish/powerline-setup.fish
+        powerline-setup
+    end
 
-# Include FZF (if exists)
-if test -d /opt/fzf
-    set fish_function_path $fish_function_path "/opt/fzf/shell/key-bindings.fish"
-    set PATH $PATH /opt/fzf/bin
-    source /opt/fzf/shell/key-bindings.fish
-    # This must be wrapped with function to be activated later
-    function fish_user_key_bindings
-        fzf_key_bindings
+    # Include FZF (if exists)
+    if test -d /opt/fzf
+        set fish_function_path $fish_function_path "/opt/fzf/shell/key-bindings.fish"
+        set PATH $PATH /opt/fzf/bin
+        source /opt/fzf/shell/key-bindings.fish
+        # This must be wrapped with function to be activated later
+        function fish_user_key_bindings
+            fzf_key_bindings
+        end
     end
 end
 
