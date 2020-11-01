9d11fe1154ac9af64a8dfbc4ff4c12adf78cd397
Peter Golovachev <petro@petro.ws>
2020-11-01 22:08:04 +0100

    Force libreoffice to use QT5 app theme

--- saltstack/salt/files/linux-config/home/sessionrc.sh
+++ saltstack/salt/files/linux-config/home/sessionrc.sh
@@ -1,4 +1,6 @@
 export QT_QPA_PLATFORMTHEME=qt5ct
 export GDK_DPI_SCALE={{ pillar.xsession.gtk_scale }}
+# Force libreoffice to use QT5 theme
+export SAL_USE_VCLPLUGIN=qt5
 
 {{ pillar.xsession.script|default('') }}
