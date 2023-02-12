dae8efc56d1cdac03398540f593932647cf44c40
Peter Golovachev <petro@petro.ws>
2023-02-12 10:22:03 +0100

    CMK: add update agent script

---
+++ bin/cmk-download-agent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+FILENAME=$(grep -o -P 'check-mk-agent_.*\.deb' saltstack/pillar/default.sls | head -n 1)
+
+mkdir -p saltstack/salt/packages
+
+wget https://cmk.system.pws/cmk/check_mk/agents/$FILENAME -O saltstack/salt/packages/$FILENAME
