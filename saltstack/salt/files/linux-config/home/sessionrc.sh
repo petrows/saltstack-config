7dc03afd39867ac51e6e806d39316fd295052d3e
Peter Golovachev <petro@petro.ws>
2023-06-18 18:11:27 +0200

    Rework desktop startup scripts
    Start mount and network before Desktop, to have correct tools behavior

--- saltstack/salt/files/linux-config/home/sessionrc.sh
+++ saltstack/salt/files/linux-config/home/sessionrc.sh
@@ -5,4 +5,9 @@ export SAL_USE_VCLPLUGIN=gtk3
 # Default apps scaling for HDPI
 export GDK_DPI_SCALE={{ pillar.xsession.gtk_scale }}
 
+# Start private autorun (if exists)
+if [[ -x $HOME/bin/autorun ]]; then
+    $HOME/bin/autorun
+fi
+
 {{ pillar.xsession.script|default('') }}
