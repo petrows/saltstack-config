1c29b9d2614130341a093df4d346e5d3a6d1571f
Petro <petro@petro.ws>
2024-05-28 15:06:47 +0200

    Add QT scale factor

--- saltstack/salt/files/linux-config/home/sessionrc.sh
+++ saltstack/salt/files/linux-config/home/sessionrc.sh
@@ -8,6 +8,8 @@ export SAL_USE_VCLPLUGIN=gtk3
 # Default apps scaling for HDPI
 export GDK_DPI_SCALE={{ pillar.xsession.gtk_scale }}
 export QT_FONT_DPI={{ pillar.xsession.qt_dpi }}
+export QT_AUTO_SCREEN_SET_FACTOR=0
+export QT_SCALE_FACTOR=1
 
 # Ask python to use system CA bundle
 export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
